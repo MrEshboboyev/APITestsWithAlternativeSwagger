55e04e4d07c69ceeb4bd6222fb00d8c7ad346bca
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-02 19:11:29 +0500

    api.html : apiDescriptionUrl replaced "/docs/api/openapi.json"; Miving .NET 9.0 ; Adding Package Microsoft.AspnetCore.OpenApi; Configure openapi in Program.cs;

--- APITestsWithAlternativeSwagger/Program.cs
+++ APITestsWithAlternativeSwagger/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+// adding OpenApi services
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
@@ -15,9 +18,12 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    //app.UseSwagger();
     //app.UseSwaggerUI();
 }
+
+// using openAPI
+app.MapOpenApi("/docs/api/openapi.json");
 
 app.UseHttpsRedirection();
 
